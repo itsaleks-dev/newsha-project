89e35108e1be09631004ad13e08d8842e006040b
Aleksandr <laptop@MacBook-Air-Aleksandr.local>
2025-09-05 17:25:52 +0300

    added folders and files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
 import "./styles/main.scss";
-import row from "./assets/images/row.png";
-
-const container = document.getElementById("image-container");
-
-if (container) {
-  // Добавляем картинку
-  const img = document.createElement("img");
-  img.src = row;
-  img.alt = "row example";
-  container.appendChild(img);
-
-  // Добавляем текст
-  const message = document.createElement("h1");
-  message.textContent = "Hello Webpack + TypeScript 🚀";
-  container.appendChild(message);
-}
