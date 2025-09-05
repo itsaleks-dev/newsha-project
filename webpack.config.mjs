1d6b5dbd75e4390af3a447435cb635f656d84355
Aleksandr <laptop@MacBook-Air-Aleksandr.local>
2025-09-05 17:34:21 +0300

    added fonts breakpoints main-style and copy-webpack-plugin

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -4,72 +4,106 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const isProd = process.env.NODE_ENV === "production";
 
 export default {
-    entry: "./src/index.ts",
-    resolve: {
-        extensions: [".ts", ".js"]
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: isProd
-            ? "assets/js/[name].[contenthash:8].js"
-            : "assets/js/[name].js",
-        assetModuleFilename: "assets/[hash][ext][query]",
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.[jt]s$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            },
-            {
-                test: /\.s?css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.less$/i,
-                use: [
-                    isProd ? MiniCssExtractPlugin.loader : "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ]
-            },
-            {
-                test: /\.(woff2?|ttf|otf|eot)$/i,
-                type: "asset/resource",
-                generator: { filename: "assets/fonts/[name].[hash:8][ext]" }
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|webp)$/i,
-                type: "asset/resource",
-                generator: { filename: "assets/images/[name].[hash:8][ext]" }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: "./src/index.html" }),
-        new MiniCssExtractPlugin({
-            filename: "assets/css/[name].[contenthash:8].css",
-        }),
-        new ESLintPlugin({ extensions: ["js", "ts"] }),
-        ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : [])
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: isProd
+      ? "assets/js/[name].[contenthash:8].js"
+      : "assets/js/[name].js",
+    assetModuleFilename: "assets/[name].[hash:8][ext][query]",
+    publicPath: "", // для GitHub Pages
+    clean: true,
+  },
+  module: {
+    rules: [
+      // HTML + картинки из HTML
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          sources: {
+            list: [
+              { tag: "img", attribute: "src", type: "src" },
+              { tag: "img", attribute: "srcset", type: "srcset" },
+              { tag: "source", attribute: "srcset", type: "srcset" }
+            ],
+          },
+          minimize: isProd,
+        },
+      },
+      // TS / JS
+      {
+        test: /\.[jt]s$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      // SCSS / CSS
+      {
+        test: /\.s?css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "../" }, // css -> на уровень вверх к assets/*
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      // LESS (если нужно)
+      {
+        test: /\.less$/i,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      // Шрифты
+      {
+        test: /\.(woff2?|ttf|otf|eot)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/fonts/[name].[hash:8][ext]" },
+      },
+      // Картинки
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: "asset/resource",
+        generator: { filename: "assets/images/[name].[hash:8][ext]" },
+      },
     ],
-    devServer: {
-        static: path.resolve(__dirname, "dist"),
-        port: 5173,
-        open: true,
-        hot: true
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/[name].[contenthash:8].css",
+    }),
+    new ESLintPlugin({ extensions: ["js", "ts"] }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets/images", to: "assets/images", noErrorOnMissing: true },
+        { from: "src/favicon.ico", to: "favicon.ico", noErrorOnMissing: true }
+      ],
+    }),
+    ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : []),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 3000,
+    open: true,
+    hot: true,
+  },
 };
